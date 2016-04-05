a5cf3b7c2f38ee9d00ae2405afed0f486043edac
matt knutson <mknut001@gmail.com>
2016-04-05 16:59:12 -0700

    show rentals in city tile

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,27 +1,27 @@
 import Ember from 'ember';
 
-var rentals = [{
-  id: 1,
-  owner: "Veruca Salt",
-  city: "San Francisco",
-  type: "Estate",
-  bedrooms: 15,
-  image: "https://unpload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
-}, {
-  id:2,
-  owner: "Mike TV",
-  city: "Seattle",
-  type: "Condo",
-  bedrooms: 1,
-  image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
-}, {
-  id: 3,
-  owner: "Violet Beauregarde",
-  city: "Portland",
-  type: "Apartment",
-  bedrooms: 3,
-  image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
-}];
+// var rentals = [{
+//   id: 1,
+//   owner: "Veruca Salt",
+//   city: "San Francisco",
+//   type: "Estate",
+//   bedrooms: 15,
+//   image: "https://unpload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+// }, {
+//   id:2,
+//   owner: "Mike TV",
+//   city: "Seattle",
+//   type: "Condo",
+//   bedrooms: 1,
+//   image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
+// }, {
+//   id: 3,
+//   owner: "Violet Beauregarde",
+//   city: "Portland",
+//   type: "Apartment",
+//   bedrooms: 3,
+//   image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
+// }];
 
 export default Ember.Route.extend({
   model(){ ///// Model hook - returns hard coded array's /////
@@ -34,7 +34,7 @@ export default Ember.Route.extend({
   },
 
   actions: {
-   
+
    addCity3(params) {
      var newCity = this.store.createRecord('city', params);
      newCity.save();
